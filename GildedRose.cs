d020493b8d58c4b8e1adc32daa63b18baca07638
lack3r <andreasl.loizou@gmail.com>
2021-11-21 12:36:07 +0200

    made items public

--- GildedRose.cs
+++ GildedRose.cs
@@ -4,7 +4,7 @@ namespace csharp
 {
     public class GildedRose
     {
-        IList<Item> Items;
+        public IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
